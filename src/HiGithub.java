42ec7e9a729806f8a7a48a55f62d8fa217053223
Teng Li <1053371893@qq.com>
2019-07-28 17:34:54 +0800

    test

--- src/HiGithub.java
+++ src/HiGithub.java
@@ -1,5 +1,5 @@
 public class HiGithub {
     public static void main(String[] args){
-        System.out.println("Hi Github i am unhappy.");
+        System.out.println("Hi Github i am unhappy");
     }
 }
